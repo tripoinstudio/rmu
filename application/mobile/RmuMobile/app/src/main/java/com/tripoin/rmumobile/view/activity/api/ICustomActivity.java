95e59a8314efd548ef41ec29c6fe825c5a5a9b13
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2015-04-21 00:16:29 +0700

    Delete kata syahrial

--- application/mobile/RmuMobile/app/src/main/java/com/tripoin/rmumobile/view/activity/api/ICustomActivity.java
+++
@@ -1,9 +0,0 @@
-package com.tripoin.rmumobile.view.activity.api;
-
-/**
- * Created by Achmad Fauzi on 11/19/2014.
- * achmad.fauzi@sigma.co.id
- */
-public interface ICustomActivity extends IBaseActivity{
-
-}
