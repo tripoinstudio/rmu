c97a68984feb6c23acdd577e394326d1b84acbf8
KNIGHTMASTER <KNIGHTMASTER@users.noreply.github.com>
2015-04-30 21:30:56 +0700

    USER DTO refactoring

--- application/rmu-mobile/rmu/app/src/main/java/com/tripoin/rmu/model/DTO/user/UserDTO.java
+++ application/rmu-mobile/rmu/app/src/main/java/com/tripoin/rmu/model/DTO/user/UserDTO.java
@@ -1,28 +1,42 @@
 package com.tripoin.rmu.model.DTO.user;
 
+import android.os.Parcel;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.tripoin.rmu.model.DTO.BaseRESTDTO;
+import com.tripoin.rmu.model.base.impl.BaseRESTDTO;
+import com.tripoin.rmu.model.DTO.master.MasterVersion;
 import com.tripoin.rmu.model.DTO.order_header.OrderHeaderItemDTO;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * Created by Ridla on 4/18/2015.
  */
-public class UserDTO extends BaseRESTDTO {
+public class UserDTO extends BaseRESTDTO /*implements Parcelable*/ implements Serializable{
 
     @JsonProperty("security_user")
-    private UserItemDTO userItemDTOs;
+    private UserItemDTO userItemDTO;
 
     @JsonProperty("trx_order_header")
     private ArrayList<OrderHeaderItemDTO> orderHeaderItemDTOs;
 
-    public UserItemDTO getUserItemDTOs() {
-        return userItemDTOs;
+    @JsonProperty("master_version")
+    private ArrayList<MasterVersion> masterVersions;
+
+    /*public UserDTO(Parcel in) {
+        readFromParcel(in);
+    }*/
+
+    public UserDTO() {
     }
 
-    public void setUserItemDTOs(UserItemDTO userItemDTOs) {
-        this.userItemDTOs = userItemDTOs;
+    public UserItemDTO getUserItemDTO() {
+        return userItemDTO;
+    }
+
+    public void setUserItemDTO(UserItemDTO userItemDTO) {
+        this.userItemDTO = userItemDTO;
     }
 
     public ArrayList<OrderHeaderItemDTO> getOrderHeaderItemDTOs() {
@@ -33,11 +47,50 @@ public class UserDTO extends BaseRESTDTO {
         this.orderHeaderItemDTOs = orderHeaderItemDTOs;
     }
 
+    public ArrayList<MasterVersion> getMasterVersions() {
+        return masterVersions;
+    }
+
+    public void setMasterVersions(ArrayList<MasterVersion> masterVersions) {
+        this.masterVersions = masterVersions;
+    }
+
     @Override
     public String toString() {
         return "UserDTO{" +
-                "userItemDTOs=" + userItemDTOs +
+                "masterVersions=" + masterVersions +
                 ", orderHeaderItemDTOs=" + orderHeaderItemDTOs +
+                ", userItemDTO=" + userItemDTO +
                 '}';
     }
+
+/*@Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(userItemDTO);
+        dest.writeList(orderHeaderItemDTOs);
+    }*/
+
+    private void readFromParcel(Parcel in) {
+        //userItemDTO = in.readParcelable();
+    }
+
+    /*Creator creator = new Creator() {
+        @Override
+        public Object createFromParcel(Parcel source) {
+            return new UserDTO(source);
+        }
+
+        @Override
+        public Object[] newArray(int size) {
+            return new UserDTO[size];
+        }
+    };*/
+
+
+
 }
--- application/rmu-mobile/rmu/app/src/main/java/com/tripoin/rmu/model/DTO/user/UserItemDTO.java
+++ application/rmu-mobile/rmu/app/src/main/java/com/tripoin/rmu/model/DTO/user/UserItemDTO.java
@@ -1,17 +1,26 @@
 package com.tripoin.rmu.model.DTO.user;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
 
 /**
  * Created by Ridla on 4/18/2015.
  */
-public class UserItemDTO {
+public class UserItemDTO /*implements Parcelable*/ implements Serializable{
 
     @JsonProperty("user_code")
     private String userCode;
 
     @JsonProperty("role_code")
     private String roleCode;
+
+    /*public UserItemDTO(Parcel in) {
+        readFromParcel(in);
+    }*/
 
     public String getUserCode() {
         return userCode;
@@ -36,4 +45,32 @@ public class UserItemDTO {
                 ", roleCode='" + roleCode + '\'' +
                 '}';
     }
+
+    /*@Override
+    public int describeContents() {
+        return 0;
+    }*/
+
+    /*@Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(userCode);
+        dest.writeString(roleCode);
+    }*/
+
+    /*private void readFromParcel(Parcel in) {
+        userCode = in.readString();
+        roleCode = in.readString();
+    }*/
+
+    /*Creator creator = new Creator() {
+        @Override
+        public Object createFromParcel(Parcel source) {
+            return new UserItemDTO(source);
+        }
+
+        @Override
+        public Object[] newArray(int size) {
+            return new UserItemDTO[size];
+        }
+    };*/
 }
