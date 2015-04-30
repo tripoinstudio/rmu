ad2c05d0854b99a52c514968e9215d039b5947c8
KNIGHTMASTER <KNIGHTMASTER@users.noreply.github.com>
2015-04-30 21:43:11 +0700

    Repackage BaseRestDTO

---
+++ application/rmu-mobile/rmu/app/src/main/java/com/tripoin/rmu/model/base/impl/BaseRESTDTO.java
@@ -0,0 +1,11 @@
+package com.tripoin.rmu.model.base.impl;
+
+import com.tripoin.rmu.model.base.ABaseRESTDTO;
+
+/**
+ * Created by Achmad Fauzi on 11/25/2014.
+ * fauzi.knightmaster.achmad@gmail.com
+ */
+
+public class BaseRESTDTO extends ABaseRESTDTO {
+}
