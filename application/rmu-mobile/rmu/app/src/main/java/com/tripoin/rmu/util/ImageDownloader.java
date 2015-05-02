ffab3ba78992d9c6dfd8a4eb61f893c7fbf41310
KNIGHTMASTER <KNIGHTMASTER@users.noreply.github.com>
2015-05-02 11:46:36 +0700

    Download util

---
+++ application/rmu-mobile/rmu/app/src/main/java/com/tripoin/rmu/util/ImageDownloader.java
@@ -0,0 +1,50 @@
+package com.tripoin.rmu.util;
+
+import org.apache.http.util.ByteArrayBuffer;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by Achmad Fauzi on 5/2/2015 : 11:42 AM.
+ * mailto : achmad.fauzi@sigma.co.id
+ */
+public class ImageDownloader {
+
+    private String urlSource;
+    private String pathTarget;
+
+    public ImageDownloader(String urlSource, String pathTarget) {
+        this.urlSource = urlSource;
+        this.pathTarget = pathTarget;
+    }
+
+    public void downloadImage(){
+        try {
+            URL url = new URL(urlSource);
+            File file = new File(pathTarget);
+
+            URLConnection ucon = url.openConnection();
+            InputStream is = ucon.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+
+            ByteArrayBuffer baf = new ByteArrayBuffer(50);
+            int current = 0;
+            while ((current = bis.read()) != -1) {
+                baf.append((byte) current);
+            }
+
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(baf.toByteArray());
+            fos.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
