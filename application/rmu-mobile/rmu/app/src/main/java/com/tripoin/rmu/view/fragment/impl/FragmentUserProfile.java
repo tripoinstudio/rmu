cfc8a7097ba43797ee31c6d77b069a75849b94ee
anjarganteng <ganjar.snjy@gmail.com>
2015-05-03 11:55:57 +0700

    Anjar : photo edit | came blm fix

--- application/rmu-mobile/rmu/app/src/main/java/com/tripoin/rmu/view/fragment/impl/FragmentUserProfile.java
+++ application/rmu-mobile/rmu/app/src/main/java/com/tripoin/rmu/view/fragment/impl/FragmentUserProfile.java
@@ -1,11 +1,21 @@
 package com.tripoin.rmu.view.fragment.impl;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,13 +28,42 @@ import android.widget.Toast;
 import com.tripoin.rmu.R;
 import com.tripoin.rmu.util.impl.PropertyUtil;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 /**
  * Created by Ginanjar Aji Sanjaya on 4/29/2015.
  */
 
 public class FragmentUserProfile extends Fragment {
 
-    private TextView nameUser, jabatanUser, emailUser, summmaryUser, lblEditText, lblViewPhoto, lblChangePhoto;
+    private TextView nameUser, jabatanUser, emailUser, summmaryUser, lblEditText, lblViewPhoto, lblChangePhotoGallery, lblChangePhotoCamera;
     private ImageView imgUserProfile, imgNameUser, imgJabatanUser, imgEmailUser, imgSummmaryUses;
     private EditText editText;
     private PropertyUtil propertyUtil;
@@ -40,6 +79,16 @@ public class FragmentUserProfile extends Fragment {
         rootView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 
         imgUserProfile = (ImageView) rootView.findViewById(R.id.imgUserProfile);
+        imgUserProfile.setOnLongClickListener(new View.OnLongClickListener() {
+            public boolean onLongClick(View arg0) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                startActivityForResult(intent, 2);
+                return true;
+            }
+        });
+
         imgUserProfile.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,13 +99,9 @@ public class FragmentUserProfile extends Fragment {
                 alertDialogBuilder.setView(dialogView);
 
                 lblViewPhoto = (TextView) dialogView.findViewById(R.id.lbl_view_photo);
-                lblViewPhoto.setOnClickListener( new View.OnClickListener(){
+                lblViewPhoto.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-//                        FragmentMenuList fragmentMenuList = new FragmentMenuList();
-//                        FragmentManager mFragmentManager = getActivity().getSupportFragmentManager();
-//                        mFragmentManager.beginTransaction().replace(R.id.container, fragmentMenuList).commit();
-
                         LayoutInflater layoutInflater = LayoutInflater.from(rootView.getContext());
                         View dialogView = layoutInflater.inflate(R.layout.fragment_view_photo_profile, null);
 
@@ -65,6 +110,26 @@ public class FragmentUserProfile extends Fragment {
 
                         AlertDialog alertD = alertDialogBuilder.create();
                         alertD.show();
+                    }
+                });
+
+                lblChangePhotoGallery = (TextView) dialogView.findViewById(R.id.lbl_change_photo_gallery);
+                lblChangePhotoGallery.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(intent, 1);
+                    }
+                });
+
+                lblChangePhotoCamera = (TextView) dialogView.findViewById(R.id.lbl_change_photo_camera);
+                lblChangePhotoCamera.setOnClickListener( new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                        startActivityForResult(intent, 2);
                     }
                 });
 
@@ -228,6 +293,62 @@ public class FragmentUserProfile extends Fragment {
         propertyUtil = new PropertyUtil("UserProfileData.txt" , rootView.getContext());
 
         return rootView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == 1) {
+                Uri selectedImage = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getActivity().getContentResolver().query(selectedImage,filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                Log.w("path of image:", picturePath + "");
+                imgUserProfile.setImageBitmap(thumbnail);
+            } else if (requestCode == 2) {
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+                    System.gc();
+                    Bitmap bitmap;
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+                    bitmapOptions.inJustDecodeBounds = false;
+                    bitmapOptions.inPreferredConfig = Bitmap.Config.RGB_565;
+                    bitmapOptions.inDither = true;
+                    System.gc();
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(), bitmapOptions);
+                    imgUserProfile.setImageBitmap(bitmap);
+                    String path = android.os.Environment.getExternalStorageDirectory()+File.separator+"Phoenix"+File.separator+"default";
+                    f.delete();
+                    OutputStream outFile = null;
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    try {
+                        outFile = new FileOutputStream(file);
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                        outFile.flush();
+                        outFile.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
