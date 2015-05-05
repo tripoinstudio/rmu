52c82c2417b2b6e757495c12934f76789ad929a6
anjarganteng <ganjar.snjy@gmail.com>
2015-05-05 17:05:50 +0700

    Anjar : Bitmap Decoder & Setting width x height

---
+++ application/rmu-mobile/rmu/app/src/main/java/com/tripoin/rmu/util/BitmapDecoder.java
@@ -0,0 +1,37 @@
+package com.tripoin.rmu.util;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+/**
+ * Created by Ginanjar Aji Sanjaya on 5/5/2015.
+ */
+public class BitmapDecoder {
+    public static Bitmap decodeSampledBitmapFromPath(String path, int reqWidth, int reqHeight) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        Bitmap bmp = BitmapFactory.decodeFile(path, options);
+        return bmp;
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            if (width > height) {
+                inSampleSize = Math.round((float) height / (float) reqHeight);
+            } else {
+                inSampleSize = Math.round((float) width / (float) reqWidth);
+            }
+        }
+        return inSampleSize;
+    }
+}
