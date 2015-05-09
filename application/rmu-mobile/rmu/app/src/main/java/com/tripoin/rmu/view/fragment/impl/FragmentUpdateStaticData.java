b428ef50b424a24726d4de9364a3dae44b2bc330
KNIGHTMASTER <KNIGHTMASTER@users.noreply.github.com>
2015-05-10 03:08:31 +0700

    +Fauzi ++Manifest Perbaiki

--- application/rmu-mobile/rmu/app/src/main/java/com/tripoin/rmu/view/fragment/impl/FragmentUpdateStaticData.java
+++ application/rmu-mobile/rmu/app/src/main/java/com/tripoin/rmu/view/fragment/impl/FragmentUpdateStaticData.java
@@ -1,17 +1,11 @@
 package com.tripoin.rmu.view.fragment.impl;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.widget.Button;
-
-import com.RT_Printer.BluetoothPrinter.BLUETOOTH.BluetoothPrintDriver;
 
 import com.tripoin.rmu.R;
 import com.tripoin.rmu.view.fragment.base.ABaseNavigationDrawerFragment;
 
-import butterknife.InjectView;
-import butterknife.OnClick;
 
 /**
  * Created by Achmad Fauzi on 4/18/2015 : 11:32 AM.
@@ -19,14 +13,6 @@ import butterknife.OnClick;
  */
 public class FragmentUpdateStaticData extends ABaseNavigationDrawerFragment {
 
-    @InjectView(R.id.btPrintSelfTest)
-    Button btPrint;
-
-    @InjectView(R.id.btScanDevice)
-    Button btScan;
-
-    @InjectView(R.id.btConnectDevice)
-    Button btConnectDevice;
 
     public FragmentUpdateStaticData newInstance(String text){
         FragmentUpdateStaticData mFragment = new FragmentUpdateStaticData();
@@ -52,23 +38,6 @@ public class FragmentUpdateStaticData extends ABaseNavigationDrawerFragment {
     @Override
     public int getViewLayoutId() {
         return R.layout.fragment_update_static_data;
-    }
-
-    @OnClick(R.id.btScanDevice)
-    public void scan(){
-
-    }
-
-    @OnClick(R.id.btPrintSelfTest)
-    public void printSelfTest(){
-
-    }
-
-    @OnClick(R.id.btConnectDevice)
-    public void connectToDevice(){
-        Intent serverIntent = null;
-        serverIntent = new Intent(getActivity(), DeviceListActivity.class);
-        startActivityForResult(serverIntent, IBluetoothConstant.REQUEST_CONNECT_DEVICE_SECURE);
     }
 
 }
