366a53a2365f2b840c9a4478c3d94fc0b3d0ae28
KNIGHTMASTER <KNIGHTMASTER@users.noreply.github.com>
2015-05-10 03:09:06 +0700

    +Fauzi ++Manifest Perbaiki

---
+++ application/rmu-mobile/rmu/app/src/main/java/com/tripoin/rmu/view/ui/DialogEditIp.java
@@ -0,0 +1,23 @@
+package com.tripoin.rmu.view.ui;
+
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.widget.TextView;
+
+import com.tripoin.rmu.R;
+
+import butterknife.InjectView;
+
+/**
+ * Created by Achmad Fauzi on 5/6/2015 : 4:40 PM.
+ * mailto : achmad.fauzi@sigma.co.id
+ */
+public class DialogEditIp extends DialogFragment {
+
+    @InjectView(R.id.lbl_change_ip_port)
+    TextView labelEditIpPort;
+
+
+
+}
