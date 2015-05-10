9982e3773c2417ce0c5944ba24f01cbd196858ec
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2015-05-10 14:46:48 +0700

    transaction

--- application/rmu-mobile/rmu/app/src/main/java/com/tripoin/rmu/view/enumeration/ViewConstant.java
+++ application/rmu-mobile/rmu/app/src/main/java/com/tripoin/rmu/view/enumeration/ViewConstant.java
@@ -27,6 +27,7 @@ public enum ViewConstant {
 
     /*Fragment Titles*/
     FRAGMENT_ABOUT_TITLE("About Application"),
+    FRAGMENT_MENU_TITLE("Menu List"),
     FRAGMENT_ORDER_LIST_TITLE("Order List"),
     FRAGMENT_ORDER_DETAIL_TITLE("Order Detail"),
     FRAGMENT_SERVER_CONFIGURATION("Server Configuration"),
