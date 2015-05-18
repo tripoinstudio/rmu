bfe05adc140dd9626a3e9baa14e3b10ac8c09394
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2015-05-18 11:05:40 +0700

    client ws

---
+++ application/rmu-rest/tripoin-root-core/tripoin-core-web-service/src/test/java/com/tripoin/rest/test/ClientWsTest.java
@@ -0,0 +1,41 @@
+package com.tripoin.rest.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author <a href="mailto:ridla.fadilah@gmail.com">Ridla Fadilah</a>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { 
+		"classpath*:/META-INF/spring/integration/http-outbound-config.xml"})
+public class ClientWsTest implements ApplicationContextAware {
+	
+	private static transient final Logger LOGGER = LoggerFactory.getLogger(ClientWsTest.class);
+	
+	private ApplicationContext applicationContext;
+
+	public <T> T getBean(Class<T> beanType) {
+		return applicationContext.getBean(beanType);
+	}
+
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+	
+	public ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+	
+	@Test
+	public void runTest() throws Exception {
+		
+	}
+}
